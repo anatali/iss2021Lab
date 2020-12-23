5aebc57e2f485c9cd058075be0c62a1e54acb430
Antonio Natali <antonio.natali@unibo.it>
2020-12-23 15:41:25 +0100

    provabuilder

---
+++ it.unibo.builder/resources/it/unibo/utils/Utils.java
@@ -0,0 +1,12 @@
+package it.unibo.utils;
+
+public class Utils {
+
+	public static void test() {
+		System.out.println("Hello from Utils.java");
+	}
+	
+	public static void main( String[] args) {
+		test();
+	}
+}
---
+++ it.unibo.builder/src/disi/builder/utils.kt
@@ -0,0 +1,36 @@
+package disi.builder
+
+object genUtils {
+	fun test() {
+		println("Hello from Utils.kt")
+	}
+
+/*
+ genFileDir
+*/	
+ fun genFileDir( dir: String,  packageName: String,  name: String,  suffix:String ,  contents: String){
+   val sysName  = packageName.replace(".", "/")
+   var fName    = ""
+   var afterDot = ""
+	 if( suffix.length > 0  ) afterDot = "." + suffix
+	 if( sysName.length > 0 ) fName   = dir+"/"+sysName + "/" + name + afterDot
+	 else fName   = dir+"/"+ name + afterDot
+     println(" +++ generate " + fName); 
+	 val fa = java.io.File( fName )
+	 fa.writeText(contents)	  
+}	
+
+}//Object Utils
+
+object generator{
+	fun genSysRules(){
+		genUtils.genFileDir( "./src-gen",  "",  "sysRules" , "pl", builtin.sysRules )
+	}
+}//Object genRules
+
+	fun main() {	//args: Array<String>?
+		//Utils.test()
+		val path = System.getProperty("user.dir")
+		println(" +++ generate in dir:$path"  );
+		generator.genSysRules();
+	}
