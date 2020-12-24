8820559ccc63c7cc1eadb0ffed54306d3c130ae5
Antonio Natali <agoognat@gmail.com>
2020-12-24 17:17:02 +0100

    builder

--- unibo.disi.builder/demo0.pl
+++ unibo.disi.builder/demo0.pl
@@ -3,5 +3,5 @@
 %====================================================================================
 ctx(ctxdemo0, "localhost",  "TCP", "8095").
  qactor( demo, ctxdemo0, "it.unibo.demo.Demo").
-  qactor( sender, ctxdemo0, "it.unibo.sender.Sender").
-  qactor( perceiver, ctxdemo0, "it.unibo.perceiver.Perceiver").
+%%  qactor( sender, ctxdemo0, "it.unibo.sender.Sender").
+%%  qactor( perceiver, ctxdemo0, "it.unibo.perceiver.Perceiver").
