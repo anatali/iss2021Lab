d1b4e3309c38c38e96211104356cb6725b563da0
Antonio Natali <antonio.natali@unibo.it>
2021-01-02 21:26:54 +0100

    disi gradle gen from start

--- provaDisiBuilder/app/build.gradle.kts
+++ provaDisiBuilder/app/build.gradle.kts
@@ -9,7 +9,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
-	id("unibo.disi.builder") version "1.0"
+	id("unibo.disi.builder") //version "1.0"
 }
 
 repositories {
