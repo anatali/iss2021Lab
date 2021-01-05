cab0151c4587d9c1e12c6cce3a0c06bbd03e8840
Antonio Natali <agoognat@gmail.com>
2021-01-05 17:19:59 +0100

    clean repo

--- testBuilder/demo0.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% demo0 description   
-%====================================================================================
-system( demo0, msgdriven ).
-
-context(ctxdemo0, "localhost",  "TCP", "8095").
- qactor( demo0, ctxdemo0, _).
- %% qactor( sender, ctxdemo0, "it.unibo.sender.sender").
- %% qactor( perceiver, ctxdemo0, "it.unibo.perceiver.perceiver").
--- testBuilder/demo1.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% demo1 description
-%====================================================================================
-system( demo1, msgbased ).
-
-context(ctxdemo1, "localhost",  "TCP", "8095").
- qactor( demo1, ctxdemo1, "it.unibo.demo1.demo1").
- %% qactor( sender, ctxdemo1, "it.unibo.sender.sender").
- %% qactor( perceiver, ctxdemo1, "it.unibo.perceiver.perceiver").
