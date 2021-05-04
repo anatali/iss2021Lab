5baf79fa4218eeeb758baeae55d3e47fe4c3652f
Antonio Natali <agoognat@gmail.com>
2021-05-04 18:09:14 +0200

    kkk

--- kkk/sysexample.pl
+++ kkk/sysexample.pl
@@ -3,4 +3,4 @@
 %====================================================================================
 context(ctxsysexample, "localhost",  "TCP", "8072").
  qactor( qa0, ctxsysexample, "it.unibo.qa0.Qa0").
-  qactor( qacoded, ctxsysexample, "resources.qacoded").
+  qactor( qacoded, ctxsysexample, "kotlinCode.qacoded").
